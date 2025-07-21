f42899069f78bd9f7f2cd9567d5c9fc616f4d808
kalinybatista <96453038+kalinybatista@users.noreply.github.com>
2025-07-21 20:47:19 -0300

    Add NotasFiscaisSystem/Program.cs 1753141639806

---
+++ NotasFiscaisSystem/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using NotasFiscaisSystem.Data;
+using NotasFiscaisSystem.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Entity Framework
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services
+builder.Services.AddScoped<INotaFiscalService, NotaFiscalService>();
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
